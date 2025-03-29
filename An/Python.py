d4fb0ddf73a4d3a192113d47c8236b553766eaea
VinhAnNguyenDuc <vincentvangogh096@gmail.com>
2025-03-29 15:21:36 +0700

    Add files via upload

---
+++ An/Python.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.ttk import Combobox
+# from PIL import Image,ImageTk
+import json
+from tkinter import Menu
+window = Tk()
+window.title("Đăng ký tài khoản")
+window.geometry("1366x768")
+window.configure(background="light gray")
+window.mainloop()
